d85f785b72bbefc80bc9201ba00318fa9e45e5d8
Justin Upson <jupson@rocketmail.com>
2025-07-25 13:46:59 +0100

    order class setup

---
+++ order_book_simulator/order.py
@@ -0,0 +1,20 @@
+import uuid
+from datetime import datetime
+
+class Order:
+    def __init__(self, symbol, side, type_, price, quantity):
+        self.id = str(uuid.uuid4())
+        self.timestamp = datetime.utcnow()
+        self.symbol = symbol  # which stock e.g. AAPL
+        self.side = side  # which of 'buy' or 'sell'
+        self.type = type_  # which of 'market' or 'limit'
+        self.price = price
+        self.quantity = quantity  # how much is intended to be traded
+        self.filled_qty = 0  # tracking how much of the order has been executed
+        self.status = 'open'  # indicates if the order is active, awaiting execution, or been cancelled
+
+    def remaining(self):
+        return self.quantity - self.filled_qty
+
+    def is_filled(self):
+        return self.remaining() == 0
